fedb30ee5ded3ccbe3e56dfd04638fbd124da837
Huitzi <e.huitzilopochtli@gmail.com>
2020-09-07 23:24:46 -0500

    Menu agregado

--- programa1.cpp
+++ programa1.cpp
@@ -16,9 +16,46 @@ void crearArbolBinario(int, int);
 
 int main(){
 
-	crearArbolBinario(5, 0);
-	sleep(12);
+	int eleccion;
+
+
+	printf("--------------------------------\n");
+	printf("Seleccione una opcion\n");
+	printf("\t1 Abanico de procesos\n");
+	printf("\t2 Lista de procesos cada uno con un descendiente\n");
+	printf("\t3 Arbol binario de procesos\n\n\t");
+	
+
+	scanf("%d", &eleccion);
+	int e;
+	switch(eleccion){
+		case 1:
+
+			printf("\n¿Cuantos hijos desea que tenga el abanico?\n");
+			scanf("%d", &e);
+			printf("\n Creando abanico...\nen otra terminal use el comando ps lf\n\n");
+			crearHijosMismoPadre(e);
+			break;
+
+		case 2:
+			printf("\n¿Cuantos elemetos desea que tenga la lista de procesos?\n");
+			scanf("%d", &e);
+			printf("\n creado lista...\nen otra terminal use el comando ps lf\n\n");
+			crearGeneraciones(e);
+			break;
+
+		case 3:
+			printf("\n¿Cuantos niveles desea que tenga el arbol binario ?\n");
+			scanf("%d", &e);
+			printf("\n creado arbol...\nen otra terminal use el comando ps lf\n\n");
+			crearArbolBinario(e, 0);
+			break;
+	}
+	
+
+	sleep(10);
 	esperarHijos();
+	
 	
 }
 
@@ -26,7 +63,7 @@ int main(){
 
 pid_t crearGeneraciones(int generaciones){
 	pid_t p=0;
-	for(int i=1; i<=generaciones;i++){
+	for(int i=1; i<generaciones;i++){
 		if(p==0){
 			p=fork();
 		}
